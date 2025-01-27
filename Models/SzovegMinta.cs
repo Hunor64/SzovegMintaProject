d14f7d733b20ae118882208472dc4d7ed90d249b
hunor64 <nhunor06@gmail.com>
2025-01-27 08:09:11 +0100

    Class update

--- Models/SzovegMinta.cs
+++ Models/SzovegMinta.cs
@@ -8,5 +8,24 @@ namespace SzovegMintaProject.Models
 {
     public class SzovegMinta
     {
+        private string elsoTag, masodikTag;
+        private byte ismetlodesSzam;
+
+        private bool vanZaroTag = false;
+
+        public SzovegMinta(string elsoTag, string masodikTag, byte ismetlodesSzam, bool vanZaroTag)
+        {
+            this.elsoTag = elsoTag;
+            this.masodikTag = masodikTag;
+            this.ismetlodesSzam = ismetlodesSzam;
+            this.vanZaroTag = vanZaroTag;
+        }
+
+        public SzovegMinta(string elsoTag, string masodikTag, byte ismetlodesSzam)
+        {
+            this.elsoTag = elsoTag;
+            this.masodikTag = masodikTag;
+            this.ismetlodesSzam = ismetlodesSzam;
+        }
     }
 }
