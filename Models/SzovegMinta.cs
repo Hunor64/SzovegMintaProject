74bb500e59eb6c468c9fc4d6a41b7184ea9ce37f
hunor64 <nhunor06@gmail.com>
2025-01-27 08:45:37 +0100

    Works

--- Models/SzovegMinta.cs
+++ Models/SzovegMinta.cs
@@ -10,22 +10,54 @@ namespace SzovegMintaProject.Models
     {
         private string elsoTag, masodikTag;
         private byte ismetlodesSzam;
+        private bool vanZaroTag;
 
-        private bool vanZaroTag = false;
-
-        public SzovegMinta(string elsoTag, string masodikTag, byte ismetlodesSzam, bool vanZaroTag)
+        public SzovegMinta(string elsoTag, string masodikTag, byte ismetlodesSzam, bool vanZaroTag = false)
         {
+            if (ismetlodesSzam == 0 || elsoTag == "" || masodikTag == "") 
+            {
+                throw new ArgumentException();
+            }
+
             this.elsoTag = elsoTag;
             this.masodikTag = masodikTag;
             this.ismetlodesSzam = ismetlodesSzam;
             this.vanZaroTag = vanZaroTag;
         }
 
-        public SzovegMinta(string elsoTag, string masodikTag, byte ismetlodesSzam)
+        public string Minta()
         {
-            this.elsoTag = elsoTag;
-            this.masodikTag = masodikTag;
-            this.ismetlodesSzam = ismetlodesSzam;
+            string eredmeny = "";
+
+            for (int i = 0; i < ismetlodesSzam; i++)
+            {
+                eredmeny += elsoTag;
+                if (i == ismetlodesSzam-1 && !vanZaroTag)
+                {
+                    break;
+                }
+                eredmeny += masodikTag;
+
+            }
+
+            return eredmeny;
+        }
+        public string ForditottMinta()
+        {
+            string eredmeny = "";
+
+            for (int i = 0; i < ismetlodesSzam; i++)
+            {
+                eredmeny += masodikTag;
+                if (i == ismetlodesSzam - 1 && !vanZaroTag)
+                {
+                    break;
+                }
+                eredmeny += elsoTag;
+
+            }
+
+            return eredmeny;
         }
     }
 }
